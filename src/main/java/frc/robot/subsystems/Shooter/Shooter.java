9ff2fbd081335a8322768c2d6ef4ba5280e215f3
PARTsMego <team3492@gmail.com>
2026-01-16 18:05:36 -0500

    Base Code for Shooter

---
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems.Shooter;
+
+import frc.robot.util.PARTs.Classes.Abstracts.PARTsSubsystem;
+
+public class Shooter extends PARTsSubsystem{
+
+    @Override
+    public void outputTelemetry() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'outputTelemetry'");
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stop'");
+    }
+
+    @Override
+    public void reset() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'reset'");
+    }
+
+    @Override
+    public void log() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'log'");
+    }
+    
+}
---
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterPhys.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems.Shooter;
+
+import com.ctre.phoenix6.sim.TalonFXSimState.MotorType;
+import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+public class ShooterPhys extends Shooter {
+    protected final SparkMax rightMotor;
+    protected final SparkMax leftMotor;
+
+    protected final RelativeEncoder leftEncoder;
+    protected final RelativeEncoder rightEncoder;
+
+    public ShooterPhys() {
+        super();
+
+        SparkMaxConfig shooterConfig = new SparkMaxConfig();
+        shooterConfig.idleMode(IdleMode.kBrake);
+
+        leftMotor = new SparkMax(0, com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless);
+        leftEncoder = leftMotor.getEncoder();
+        leftMotor.configure(shooterConfig, com.revrobotics.ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
+                
+        rightMotor = new SparkMax(0, com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless);
+        rightEncoder = rightMotor.getEncoder();
+        rightMotor.configure(shooterConfig.follow(leftMotor, true), com.revrobotics.ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
+    }
+    protected void setSpeed(double speed) {
+        leftMotor.set(speed);
+    }
+}
