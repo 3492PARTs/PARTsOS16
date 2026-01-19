69d692e67a00921d517591e1d82c1b72059638f3
NicholasW <nicholaswinarta@gmail.com>
2026-01-19 12:03:54 -0500

    kicker subsystem created

---
+++ src/main/java/frc/robot/subsystems/Kicker/Kicker.java
@@ -0,0 +1,80 @@
+package frc.robot.subsystems.Kicker;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.states.KickerState;
+import frc.robot.util.PARTs.Classes.PARTsCommandUtils;
+import frc.robot.util.PARTs.Classes.Abstracts.PARTsSubsystem;
+
+public abstract class Kicker extends PARTsSubsystem {
+
+    private KickerState kickerState = KickerState.IDLE;
+
+    public Kicker() {
+        super("Kicker");
+    }
+
+    // region Generic Subsystem Functions
+    @Override
+    public void outputTelemetry() {
+        partsNT.putString("Kicker State", kickerState.toString());
+    }
+
+    @Override
+    public void stop() {
+        kickerState = KickerState.DISABLED;
+    }
+
+    @Override
+    public void reset() {
+        kickerState = KickerState.IDLE;
+    }
+
+    @Override
+    public void log() {
+        partsLogger.logString("Kicker State", kickerState.toString());
+    }
+    // endregion
+
+    @Override
+    public void periodic() {
+        switch (kickerState) {
+            case ROLLING:
+                setSpeed(kickerState.getSpeed());
+                break;
+            case DISABLED:
+                setSpeed(kickerState.getSpeed());
+                break;
+            case IDLE:
+                setSpeed(kickerState.getSpeed());
+                break;
+            default:
+                setSpeed(0);
+                break;
+        }
+    }
+
+    // region Custom Public Functions
+    /**
+     * Sets the speed of the Kicker.
+     * 
+     * @param speed The speed between <code>-1.0</code> and <code>1.0</code>.
+     */
+    protected abstract void setSpeed(double speed);
+
+    public KickerState getState() {
+        return kickerState;
+    }
+
+    public Command roll() {
+        return PARTsCommandUtils.setCommandName("Command Roll", this.runOnce(() -> {
+            kickerState = KickerState.ROLLING;
+        }));
+    }
+
+    public Command idle() {
+        return PARTsCommandUtils.setCommandName("Command Idle", this.runOnce(() -> {
+            kickerState = KickerState.IDLE;
+        }));
+    }
+}
+// endregion
