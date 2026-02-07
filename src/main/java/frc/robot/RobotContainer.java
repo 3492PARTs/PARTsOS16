6988c711dd8261db14aab7d3007793b49cab9fdd
team3492backup <team3492@gmail.com>
2026-02-07 16:14:37 -0500

    Automatically angles towards target while moving

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -41,6 +41,8 @@ import frc.robot.subsystems.Drivetrain.PARTsDrivetrain;
 import frc.robot.subsystems.Shooter.Shooter;
 import frc.robot.subsystems.Shooter.ShooterPhys;
 import frc.robot.subsystems.Shooter.ShooterSim;
+import frc.robot.util.Field;
+
 import org.parts3492.partslib.input.PARTsButtonBoxController;
 import org.parts3492.partslib.input.PARTsCommandController;
 import org.parts3492.partslib.input.PARTsController.ControllerType;
@@ -118,7 +120,8 @@ public class RobotContainer {
 
         // reset the field-centric heading on left bumper press
         driveController.leftBumper().onTrue(drivetrain.commandSeedFieldCentric());
-        driveController.x().whileTrue(drivetrain.commandPathFindToPath("Circleish"));
+
+        driveController.x().onTrue(drivetrain.targetPoseCommand(() -> Field.blueHubCenter, () -> driveController.y().getAsBoolean()));
 
         /*
          * if (RobotConstants.DEBUGGING) {
--- src/main/java/frc/robot/constants/CameraConstants.java
+++ src/main/java/frc/robot/constants/CameraConstants.java
@@ -10,9 +10,9 @@ import edu.wpi.first.math.util.Units;
 public interface CameraConstants {
     public enum CameraName {
         LEFT_FRONT_CAMERA("limelight-frankie"),
-        LEFT_SIDE_CAMERA("limelight-sue"),
+        LEFT_SIDE_CAMERA("limelight-johnny"),
         RIGHT_FRONT_CAMERA("limelight-ben"),
-        RIGHT_SIDE_CAMERA("limelight-johnny"),
+        RIGHT_SIDE_CAMERA("limelight-sue"),
         FRONT_CENTER_CAMERA("limelight-reed");
 
         private String cameraName = "";
@@ -43,24 +43,22 @@ public interface CameraConstants {
 
     public Camera[] LimelightCameras = new Camera[] {
             new Camera(CameraName.LEFT_FRONT_CAMERA.getCameraName(),
-                    new Pose3d(.343, -.229, .216, // meters
+                    new Pose3d(.343, .1905, .22225, // meters
                             new Rotation3d(0, Units.degreesToRadians(15), 0)),
                     false),
             new Camera(CameraName.LEFT_SIDE_CAMERA.getCameraName(),
-                    new Pose3d(.21, -.368, .216, // meters
+                    new Pose3d(.22225, .3302, .22225, // meters
                             new Rotation3d(0, Units.degreesToRadians(15),
-                                    Units.degreesToRadians(-90))),
+                                    Units.degreesToRadians(90))),
                     false),
             new Camera(CameraName.RIGHT_FRONT_CAMERA.getCameraName(),
                     new Pose3d(.343, -.1905, .22225, // meters
                             new Rotation3d(0, Units.degreesToRadians(15), 0)),
-                    true),
+                    false),
             new Camera(CameraName.RIGHT_SIDE_CAMERA.getCameraName(),
                     new Pose3d(.22225, -.3302, .22225, // meters
                             new Rotation3d(0, Units.degreesToRadians(15),
-                                    Units.degreesToRadians(-90))),
-                                    
-                                    
+                                    Units.degreesToRadians(-90))),        
                     true),
             new Camera(CameraName.FRONT_CENTER_CAMERA.getCameraName(),
                     new Pose3d(-.265, 0, .545, // meters
