db0e13cdb6fd6b819d71032cecc071a0be977eff
team3492backup <team3492@gmail.com>
2026-02-15 14:36:10 -0500

    Add motor ID.

--- src/main/java/frc/robot/constants/KickerConstants.java
+++ src/main/java/frc/robot/constants/KickerConstants.java
@@ -2,5 +2,5 @@ package frc.robot.constants;
 
 public class KickerConstants {
     public static final double KICKER_SPEED = 0.1; // TBD
-    public static final int KICKER_MOTOR_ID = 0; // TBD
+    public static final int KICKER_MOTOR_ID = 34;
 }
