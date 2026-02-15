b1c9815cafe3a01e3134060fa91d005b0843cd89
team3492backup <team3492@gmail.com>
2026-02-15 14:39:05 -0500

    Add motor IDs.

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -1,6 +1,6 @@
 package frc.robot.constants;
 
 public class IntakeConstants {
-    public static final int INTAKE_MOTOR_ID = 0;
-    public static final int PIVOT_MOTOR_ID = 0;
+    public static final int INTAKE_MOTOR_ID = 36;
+    public static final int PIVOT_MOTOR_ID = 38;
 }
