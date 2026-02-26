0e0ca572e1d3c0649950a65806fa8af4257d60af
James <bucheckerr@gmail.com>
2026-02-26 00:20:12 -0500

    Updated shoot command (Not Done)

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -6,10 +6,15 @@ import org.parts3492.partslib.command.PARTsCommandUtils;
 import org.parts3492.partslib.command.PARTsSubsystem;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.RepeatCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
+import frc.robot.states.KickerState;
+import frc.robot.states.ShooterState;
 import frc.robot.subsystems.Hopper.Hopper;
 import frc.robot.subsystems.Intake.Intake;
 import frc.robot.subsystems.Kicker.Kicker;
@@ -35,6 +40,52 @@ public class Superstructure extends PARTsSubsystem {
      * lift up pivot arm, roll hopper, roll kicker, shoot. Only happens if turret has valid angle
      */
     public Command shoot(BooleanSupplier end) {
+
+        return PARTsCommandUtils.setCommandName("Superstructure.shoot",
+            
+            Commands.sequence(
+                // Initial startup
+                Commands.parallel(
+                    // Start tracking the hub.
+                    turret.track(),
+                    // Push intake in to drive balls further into the hopper.
+                    intake.intakeShooting(),
+                    // Feed the balls into the kicker.
+                    hopper.roll()
+                )
+
+                .andThen(Commands.repeatingSequence(
+
+                    new ConditionalCommand(shooter.shoot(), shooter.idle(), turret::isValidAngle)
+                    .onlyIf(() -> { return shooter.getState() != ShooterState.SHOOTING; }),
+
+                    
+                    Commands.waitUntil(shooter.atSetpoint()).andThen(
+                        kicker.roll()
+                        .onlyIf(() -> { return kicker.getState() != KickerState.ROLLING; })
+                    ),
+
+                    Commands.waitUntil(() -> { return !shooter.atSetpoint().getAsBoolean(); })
+                        .andThen(kicker.idle()
+                        .onlyIf(() -> { return kicker.getState() == KickerState.ROLLING; })
+                    )
+                )
+                .until(end)),
+                
+                // Make sure to cancel and reset if we're forced to end.
+                Commands.waitUntil(end).andThen(
+                    Commands.runOnce(() -> {
+                        turret.reset();
+                        intake.reset();
+                        hopper.reset();
+                        kicker.reset();
+                        shooter.reset();
+                    }, hopper, intake, kicker, shooter, turret)
+                )
+            )
+        );
+
+        /*
         return PARTsCommandUtils
                 .setCommandName("Superstructure.shoot",
                         Commands.parallel(turret.track(), intake.intakeShooting(), hopper.roll(), kicker.roll(),
@@ -47,6 +98,7 @@ public class Superstructure extends PARTsSubsystem {
                     kicker.reset();
                     shooter.reset();
                 }, hopper, intake, kicker, shooter, turret));
+        */
     }
 
     @Override
