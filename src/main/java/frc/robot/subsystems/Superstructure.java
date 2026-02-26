b27b5d1136dc51de19c5cba4c90a8ba9568a76dd
Dylan Winarta <dylanwinarta@gmail.com>
2026-02-25 20:24:16 -0500

    initial test

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -10,11 +10,13 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
+import frc.robot.subsystems.Drivetrain.PARTsDrivetrain;
 import frc.robot.subsystems.Hopper.Hopper;
 import frc.robot.subsystems.Intake.Intake;
 import frc.robot.subsystems.Kicker.Kicker;
 import frc.robot.subsystems.Shooter.Shooter;
 import frc.robot.subsystems.Turret.Turret;
+import frc.robot.util.Field;
 
 public class Superstructure extends PARTsSubsystem {
     private final Hopper hopper;
@@ -22,13 +24,15 @@ public class Superstructure extends PARTsSubsystem {
     private final Kicker kicker;
     private final Shooter shooter;
     private final Turret turret;
+    private final PARTsDrivetrain drivetrain;
 
-    public Superstructure(Hopper hopper, Intake intake, Kicker kicker, Shooter shooter, Turret turret) {
+    public Superstructure(Hopper hopper, Intake intake, Kicker kicker, Shooter shooter, Turret turret, PARTsDrivetrain drivetrain) {
         this.hopper = hopper;
         this.intake = intake;
         this.kicker = kicker;
         this.shooter = shooter;
         this.turret = turret;
+        this.drivetrain = drivetrain;
     }
 
     /**
@@ -38,7 +42,7 @@ public class Superstructure extends PARTsSubsystem {
         return PARTsCommandUtils
                 .setCommandName("Superstructure.shoot",
                         Commands.parallel(turret.track(), intake.intakeShooting(), hopper.roll(), kicker.roll(),
-                                shooter.shoot()).onlyIf(turret::isValidAngle))
+                                shooter.shoot()).onlyIf(() -> turret.isValidAngle() && Field.isInAllianceZone(drivetrain.getPose())))
                 .andThen(new WaitUntilCommand(() -> !turret.isValidAngle() || end.getAsBoolean()))
                 .andThen(Commands.runOnce(() -> {
                     turret.reset();
