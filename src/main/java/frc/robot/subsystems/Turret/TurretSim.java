6a0de3e649feec11e3f3acc00fb6c9d51de4fbd0
Rowan <bucheckerr@gmail.com>
2026-03-22 09:24:08 -0400

    Sim code that is logically simmed (no motor sim)

--- src/main/java/frc/robot/subsystems/Turret/TurretSim.java
+++ src/main/java/frc/robot/subsystems/Turret/TurretSim.java
@@ -3,10 +3,14 @@ package frc.robot.subsystems.Turret;
 import java.util.function.Supplier;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.RobotController;
 import frc.robot.subsystems.Drivetrain.PARTsDrivetrain;
 
 public class TurretSim extends Turret {
     //protected final TalonFX turretMotor;
+
+    double voltage = 0;
+    double angle = 0;
 
     public TurretSim(Supplier<Pose2d> robotPoseSupplier, PARTsDrivetrain drivetrain) {
         super(robotPoseSupplier, drivetrain);
@@ -14,19 +18,27 @@ public class TurretSim extends Turret {
 
     @Override
     protected void setSpeed(double speed) {
+        voltage = speed * RobotController.getBatteryVoltage();
     }
 
     @Override
     protected void setVoltage(double voltage) {
+        this.voltage = voltage;
     }
 
     @Override
     protected double getVoltage() {
-        return 0;
+        return voltage;
     }
 
     @Override
     protected double getAngle() {
-        return 0;
+        return angle;
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        super.simulationPeriodic();
+        angle = getAngleToTarget(getTargetPose());
     }
 }
