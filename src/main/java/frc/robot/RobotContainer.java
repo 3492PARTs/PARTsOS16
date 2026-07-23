9945032414a333a7fbbe314bc02d8e2b5661f792
Brandon <brandon@bduke.dev>
2026-07-23 12:31:23 -0400

    add paths

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -226,7 +226,7 @@ public class RobotContainer {
     configureAutonomousCommands();
 
     // Configure the button bindings
-    configureButtonBindings();
+    configureDriveBindings();
     configureShooterBindings();
     configureCandleBindings();
     configureHopperBindings();
@@ -252,7 +252,7 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {
+  private void configureDriveBindings() {
     // Default command, normal field-relative drive
     drive.setDefaultCommand(
         DriveCommands.joystickDrive(
